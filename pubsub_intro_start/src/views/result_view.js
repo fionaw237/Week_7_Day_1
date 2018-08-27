01aa91912884ec011c40f0b73b00175e2c39de5d
Fiona Wilson <fionaw237@gmail.com>
2018-08-27 15:26:56 +0100

    Adds intro to PubSub

---
+++ pubsub_intro_start/src/views/result_view.js
@@ -0,0 +1,24 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+const ResultView = function(){
+
+}
+
+ResultView.prototype.bindEvents = function(){
+  PubSub.subscribe("PrimeChecker:result", (event) => {
+    const result = event.detail;
+    this.updateView(result);
+  })
+}
+
+ResultView.prototype.updateView = function(result){
+  const resultElement = document.querySelector('#result');
+  if(result){
+    resultElement.textContent = "Yes! It's a prime number."
+  }
+  else{
+    resultElement.textContent = "No. It's not a prime number."
+  }
+}
+
+module.exports = ResultView;
